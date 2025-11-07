3f4ff0c4d86b732f4fb9b19ea2e8692ee059410f
alexdiguidommt <alessandro.di-guido@mmtdigital.co.uk>
2025-11-07 13:48:42 +0000

    init commitial commit project setup.

---
+++ Payroc.App/Payroc.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using Payroc.App.LoadBalancer;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// 1999 Load Balancer - Keep it simple!
+var loadBalancer = new LoadBalancer();
+
+// Handle Ctrl+C gracefully
+Console.CancelKeyPress += (sender, args) =>
+{
+    args.Cancel = true;
+    loadBalancer.Stop();
+};
+
+try
+{
+    await loadBalancer.StartAsync();
+}
+catch (Exception ex)
+{
+    Logger.Error($"Fatal error: {ex.Message}");
+    return 1;
+}
+
+return 0;
